563bdd070483b2e40d9d6b5b2b714fed98e02d6b
Steven A. Goodman <steven.a.goodman@outlook.com>
2024-12-13 06:32:06 -0500

    experiment with contrast drawings

--- ImgAnalysis/dist/path_creator/CreateImgPath.js
+++ ImgAnalysis/dist/path_creator/CreateImgPath.js
@@ -36,7 +36,7 @@ function createCanvasFromRGBAData(data, width, height) {
     return canvas;
 }
 function getSurroundingPixels(pixel_index, pixels_array, width, height, conv_array) {
-    console.log(pixel_index);
+    // console.log(pixel_index)
     if (Math.sqrt(conv_array.length) % 1 != 0)
         throw new Error("convolutional array is not square");
     if (Math.sqrt(conv_array.length) % 2 == 0)
@@ -44,7 +44,8 @@ function getSurroundingPixels(pixel_index, pixels_array, width, height, conv_arr
     const radius = Math.floor(Math.sqrt(conv_array.length) / 2);
     var surrounding_pixels = new Array(conv_array.length);
     var [row, column] = [Math.floor(pixel_index / width), pixel_index % width];
-    if ((column < radius || width - column < radius) || (row < radius || height - row < radius)) {
+    // console.log(row, "| act", height)
+    if ((column < radius || width - column < radius) || (row < radius || (height - 1) - row <= radius)) {
         console.log("pixel is on edge, ignoring and returning " + pixels_array[pixel_index]);
         surrounding_pixels.fill(pixels_array[pixel_index]);
         return (surrounding_pixels); // if pixel is edge pixel, ignore
@@ -63,7 +64,7 @@ function applyConvolution(surrounding_pixels, conv_array) {
     if (surrounding_pixels.length != conv_array.length)
         throw Error("convolutional array must be the same size as the surrounding pixels");
     var weighted_pixels = [];
-    for (var i = 0; i < conv_array.length; i++) {
+    for (var i = 0; i < surrounding_pixels.length; i++) {
         var weighted_pix = [];
         surrounding_pixels[i].forEach(pix_comp => {
             weighted_pix.push(pix_comp * conv_array[i]);
@@ -72,7 +73,7 @@ function applyConvolution(surrounding_pixels, conv_array) {
     }
     // vvv currently sum but test with max
     const conv_scaling = weighted_pixels.length * Math.max(...conv_array);
-    console.log(weighted_pixels);
+    // console.log(weighted_pixels)
     var averaged_pixel = [0, 0, 0, 0];
     weighted_pixels.forEach(pix => {
         averaged_pixel[0] += pix[0] / (conv_scaling);
@@ -80,7 +81,20 @@ function applyConvolution(surrounding_pixels, conv_array) {
         averaged_pixel[2] += pix[2] / (conv_scaling);
         averaged_pixel[3] += pix[3] / (conv_scaling);
     });
+    // rounding
+    averaged_pixel = averaged_pixel.map((comp) => { return Math.floor(comp); });
     return averaged_pixel;
+}
+function applyContrastFilter(surrounding_pixels) {
+    var main_pixel = surrounding_pixels[Math.floor(surrounding_pixels.length / 2)];
+    surrounding_pixels.splice(Math.floor(surrounding_pixels.length / 2));
+    var avg_contrast = 0;
+    surrounding_pixels.forEach(pixel => {
+        avg_contrast += contrast(main_pixel, pixel);
+    });
+    avg_contrast *= 1 / surrounding_pixels.length;
+    var contrast_pix = new Array(4).fill(255 / 2 * avg_contrast + 255 / 2);
+    return contrast_pix;
 }
 function main(img, index) {
     // getting pixel array of jpg img
@@ -88,22 +102,29 @@ function main(img, index) {
     var img_pixels = Array.from(jpg_data.data);
     var grouped_img_pixels = reshape(img_pixels, jpg_data.height * jpg_data.width, 4);
     // do stuff to pixel array
-    console.log(grouped_img_pixels);
-    const convolutional_array = [
-        1, 1, 1,
-        1, 1, 1,
-        1, 1, 1
-    ];
-    var final_img_pixels = [];
+    // console.log(grouped_img_pixels)
+    // const convolutional_array = [
+    //     1,100,1,
+    //     100,10,100,
+    //     1,1,1
+    // ]
+    const convolutional_array = new Array(Math.pow(7, 2)).fill(1);
+    var convolved_img_pixels = [];
+    var contrast_img_pixels = [];
     for (var i = 0; i < grouped_img_pixels.length; i++) {
+        // var i = 430982
         var surrounding_pixels = getSurroundingPixels(i, grouped_img_pixels, jpg_data.width, jpg_data.height, convolutional_array);
         var deconvolved_pix = applyConvolution(surrounding_pixels, convolutional_array);
-        final_img_pixels.push(deconvolved_pix);
+        convolved_img_pixels.push(deconvolved_pix);
+        var contrast_pix = applyContrastFilter(surrounding_pixels);
+        contrast_img_pixels.push(contrast_pix);
     }
     // console.log(final_img_pixels)
     // // draw final pixel array
-    const canvas = createCanvasFromRGBAData(final_img_pixels, jpg_data.width, jpg_data.height); //
-    Visualize2D.workspace.appendChild(canvas);
+    const conv_canvas = createCanvasFromRGBAData(convolved_img_pixels, jpg_data.width, jpg_data.height); //
+    Visualize2D.workspace.appendChild(conv_canvas);
+    const constrast_canvas = createCanvasFromRGBAData(contrast_img_pixels, jpg_data.width, jpg_data.height); //
+    Visualize2D.workspace.appendChild(constrast_canvas);
 }
 const Testing = (runtimeVars, time) => {
     var [index] = runtimeVars;
@@ -116,7 +137,7 @@ const runtime = new Visualize2D.Runtime(Testing); // init Runtime
 runtime.CreateTicker(Visualize2D.DefaultTicker);
 runtime.varNumber("index", 0, 10000); // set desired vars
 runtime.UpdateScreen(0);
-// DEFAULT FUNCTIONS
+// GENERAL FUNCTIONS
 function reshape(array, rows, cols) {
     if (rows * cols !== array.length) {
         throw new Error("Invalid dimensions for reshape");
@@ -126,4 +147,26 @@ function reshape(array, rows, cols) {
         result.push(array.slice(i * cols, (i + 1) * cols));
     }
     return result;
+}
+// contrast ratio code; credit: kirilloid (https://stackoverflow.com/a/9733420)
+const RED = 0.2126;
+const GREEN = 0.7152;
+const BLUE = 0.0722;
+const GAMMA = 2.4;
+function luminance(rgba) {
+    var [r, g, b, _] = rgba;
+    var a = [r, g, b].map((v) => {
+        v /= 255;
+        return v <= 0.03928
+            ? v / 12.92
+            : Math.pow((v + 0.055) / 1.055, GAMMA);
+    });
+    return a[0] * RED + a[1] * GREEN + a[2] * BLUE;
+}
+function contrast(rgba1, rgba2) {
+    var lum1 = luminance(rgba1);
+    var lum2 = luminance(rgba2);
+    var brightest = Math.max(lum1, lum2);
+    var darkest = Math.min(lum1, lum2);
+    return (darkest + 0.05) / (brightest + 0.05);
 }
--- ImgAnalysis/path_creator/CreateImgPath.ts
+++ ImgAnalysis/path_creator/CreateImgPath.ts
@@ -46,7 +46,7 @@ function createCanvasFromRGBAData(data: Array<Array<number>>, width: number, hei
   }
 
 function getSurroundingPixels(pixel_index: number, pixels_array: Array<Array<number>>, width: number, height: number, conv_array: Array<number>) {
-    console.log(pixel_index)
+    // console.log(pixel_index)
     if (Math.sqrt(conv_array.length) % 1 != 0) throw new Error("convolutional array is not square")
     if (Math.sqrt(conv_array.length) % 2 == 0) throw new Error("convolutional array side length must be odd")
 
@@ -55,7 +55,8 @@ function getSurroundingPixels(pixel_index: number, pixels_array: Array<Array<num
     var surrounding_pixels = new Array(conv_array.length)
 
     var [row, column] = [Math.floor(pixel_index / width), pixel_index % width] 
-    if ((column < radius || width - column < radius) || (row < radius || height - row < radius)) {
+    // console.log(row, "| act", height)
+    if ((column < radius || width - column < radius) || (row < radius || (height-1) - row <= radius)) {
         console.log("pixel is on edge, ignoring and returning " + pixels_array[pixel_index])
         surrounding_pixels.fill(pixels_array[pixel_index])
         return (surrounding_pixels) // if pixel is edge pixel, ignore
@@ -87,7 +88,7 @@ function applyConvolution(surrounding_pixels: Array<Array<number>>, conv_array) 
 
     // vvv currently sum but test with max
     const conv_scaling = weighted_pixels.length * Math.max(...conv_array);
-    console.log(weighted_pixels)
+    // console.log(weighted_pixels)
     var averaged_pixel = [0,0,0,0]
     weighted_pixels.forEach(pix => {
         averaged_pixel[0] += pix[0] / (conv_scaling)
@@ -95,8 +96,26 @@ function applyConvolution(surrounding_pixels: Array<Array<number>>, conv_array) 
         averaged_pixel[2] += pix[2] / (conv_scaling)
         averaged_pixel[3] += pix[3] / (conv_scaling)
     });
+    
+    // rounding
+    averaged_pixel = averaged_pixel.map((comp) => {return Math.floor(comp)})
 
     return averaged_pixel
+}
+
+function applyContrastFilter(surrounding_pixels: Array<Array<number>>) {
+    var main_pixel = surrounding_pixels[Math.floor(surrounding_pixels.length/2)] 
+    surrounding_pixels.splice(Math.floor(surrounding_pixels.length/2))
+
+    var avg_contrast = 0
+    surrounding_pixels.forEach(pixel => {
+        avg_contrast += contrast(main_pixel, pixel)
+    });
+    avg_contrast *= 1 / surrounding_pixels.length
+
+    var contrast_pix = new Array(4).fill(255/2 * avg_contrast + 255/2)
+
+    return contrast_pix
 }
 
 function main(img, index) {
@@ -107,26 +126,33 @@ function main(img, index) {
     var grouped_img_pixels = reshape(img_pixels, jpg_data.height * jpg_data.width, 4)
 
     // do stuff to pixel array
-    console.log(grouped_img_pixels)
+    // console.log(grouped_img_pixels)
 
-    const convolutional_array = [
-        1,1,1,
-        1,1,1,
-        1,1,1
-    ]
+    // const convolutional_array = [
+    //     1,100,1,
+    //     100,10,100,
+    //     1,1,1
+    // ]
+    const convolutional_array = new Array(7**2).fill(1)
 
-    var final_img_pixels = []
-    // for (var i = 0; i < grouped_img_pixels.length; i++) {
-    var i = 430899
+    var convolved_img_pixels = []
+    var contrast_img_pixels = []
+    for (var i = 0; i < grouped_img_pixels.length; i++) {
+    // var i = 430982
         var surrounding_pixels = getSurroundingPixels(i, grouped_img_pixels, jpg_data.width, jpg_data.height, convolutional_array)
+
         var deconvolved_pix = applyConvolution(surrounding_pixels, convolutional_array)
-        final_img_pixels.push(deconvolved_pix)
-    // }
+        convolved_img_pixels.push(deconvolved_pix)
+        var contrast_pix = applyContrastFilter(surrounding_pixels)
+        contrast_img_pixels.push(contrast_pix)
+    }
 
     // console.log(final_img_pixels)
     // // draw final pixel array
-    const canvas = createCanvasFromRGBAData(final_img_pixels, jpg_data.width, jpg_data.height) //
-    Visualize2D.workspace.appendChild(canvas)
+    const conv_canvas = createCanvasFromRGBAData(convolved_img_pixels, jpg_data.width, jpg_data.height) //
+    Visualize2D.workspace.appendChild(conv_canvas)
+    const constrast_canvas = createCanvasFromRGBAData(contrast_img_pixels, jpg_data.width, jpg_data.height) //
+    Visualize2D.workspace.appendChild(constrast_canvas)
 
 }
 
@@ -148,7 +174,9 @@ runtime.UpdateScreen(0)
 
 
 
-// DEFAULT FUNCTIONS
+// GENERAL FUNCTIONS
+
+
 function reshape(array: Array<any>, rows: number, cols: number): Array<Array<any>> {
     if (rows * cols !== array.length) {
       throw new Error("Invalid dimensions for reshape");
@@ -160,3 +188,29 @@ function reshape(array: Array<any>, rows: number, cols: number): Array<Array<any
     }
     return result;
 }
+
+// contrast ratio code; credit: kirilloid (https://stackoverflow.com/a/9733420)
+    const RED = 0.2126;
+    const GREEN = 0.7152;
+    const BLUE = 0.0722;
+
+    const GAMMA = 2.4;
+
+    function luminance(rgba: Array<number>) {
+        var [r, g, b, _] = rgba
+        var a = [r, g, b].map((v) => {
+            v /= 255;
+            return v <= 0.03928
+            ? v / 12.92
+            : Math.pow((v + 0.055) / 1.055, GAMMA);
+        });
+        return a[0] * RED + a[1] * GREEN + a[2] * BLUE;
+    }
+
+    function contrast(rgba1: Array<number>, rgba2: Array<number>) {
+        var lum1 = luminance(rgba1);
+        var lum2 = luminance(rgba2);
+        var brightest = Math.max(lum1, lum2);
+        var darkest = Math.min(lum1, lum2);
+        return (darkest + 0.05) / (brightest + 0.05);
+    }
